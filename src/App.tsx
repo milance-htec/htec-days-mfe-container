46887571281109c7ed63545bcf8f3a637fa69f4e
micke95 <milan.milicevic@htecgroup.com>
2020-12-27 17:58:34 +0100

    chore: add some test case

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react';
 import { LinearProgress } from '@material-ui/core';
 import ErrorBoundary from 'components/error-boundary';
+import { MnMProvider } from '@mnm-tech/provider';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -17,14 +18,16 @@ const ChildApp2 = React.lazy(() => import('mnmMfeChildApp2/App'));
 
 const App = () => {
   return (
-    <React.Suspense fallback={<LinearProgress />}>
-      <ErrorBoundary key="child-app-1">
-        <ChildApp1 />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <ChildApp2 key="child-app-2" />
-      </ErrorBoundary>
-    </React.Suspense>
+    <MnMProvider>
+      <React.Suspense fallback={<LinearProgress />}>
+        <ErrorBoundary key="child-app-1">
+          <ChildApp1 />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <ChildApp2 key="child-app-2" />
+        </ErrorBoundary>
+      </React.Suspense>
+    </MnMProvider>
   );
 };
 
