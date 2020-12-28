e834e221c1072294a7ae7ac4d6121dde7357a567
Mario Petrovic <mario.petrovic@htecgroup.com>
2020-12-28 01:07:12 +0100

    Added clear button for list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react';
 import { LinearProgress } from '@material-ui/core';
 import ErrorBoundary from 'components/error-boundary';
-import { MnMProvider } from '@mnm-tech/provider';
+import { MnMProvider, useMnM } from '@mnm-tech/provider';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,37 +16,49 @@ export const AppContext = createContext({
 //@ts-ignore
 const ChildApp1 = React.lazy(() => import('mnmMfeChildApp1/App'));
 //@ts-ignore
+const Button = React.lazy(() => import('mnmMfeChildApp1/components/Button'));
+//@ts-ignore
 const ChildApp2 = React.lazy(() => import('mnmMfeChildApp2/App'));
 //@ts-ignore
 const Image = React.lazy(() => import('mnmMfeChildApp2/components/Image'));
 
 const App = () => {
+  const { setItemList } = useMnM();
+
+  const onClearListClear = () => {
+    setItemList([]);
+  };
+
   return (
-    <MnMProvider>
-      <React.Suspense fallback={<LinearProgress />}>
-        <div className="mfe-container">
-          <span className="mfe-container__heading">Container APP</span>
-          <ErrorBoundary key="image-component">
-            <div className="mfe-container__logo">
-              <Image src="./assets/logo.png" />
-            </div>
-            <div className="mfe-container__company-logo">
-              <Image src="./assets/htec-logo.png" />
-            </div>
-          </ErrorBoundary>
-          <div className="mfe-container__child-1">
-            <ErrorBoundary key="child-app-1">
-              <ChildApp1 />
-            </ErrorBoundary>
-          </div>
-          <div className="mfe-container__child-2">
-            <ErrorBoundary key="child-app-2">
-              <ChildApp2 />
-            </ErrorBoundary>
+    <React.Suspense fallback={<LinearProgress />}>
+      <div className="mfe-container">
+        <div className="mfe-container__heading">
+          <div>Container APP</div>
+          <div>
+            <Button onClick={onClearListClear}>Clear list in Child App #2</Button>
           </div>
         </div>
-      </React.Suspense>
-    </MnMProvider>
+
+        <ErrorBoundary key="image-component">
+          <div className="mfe-container__logo">
+            <Image src="./assets/logo.png" />
+          </div>
+          <div className="mfe-container__company-logo">
+            <Image src="./assets/htec-logo.png" />
+          </div>
+        </ErrorBoundary>
+        <div className="mfe-container__child-1">
+          <ErrorBoundary key="child-app-1">
+            <ChildApp1 />
+          </ErrorBoundary>
+        </div>
+        <div className="mfe-container__child-2">
+          <ErrorBoundary key="child-app-2">
+            <ChildApp2 />
+          </ErrorBoundary>
+        </div>
+      </div>
+    </React.Suspense>
   );
 };
 
--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { MnMProvider } from '@mnm-tech/provider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MnMProvider>
+    <App />
+  </MnMProvider>,
+  document.getElementById('root'),
+);
